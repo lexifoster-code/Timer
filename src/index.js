34ff905c080519cbea6c244d04d4b60b572ff74a
lexifoster-code <70602502+lexifoster-code@users.noreply.github.com>
2023-06-27 15:32:28 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Count Down Timer!</h1>
+<div>
+<span class ="timer"> 00:00:00</span>
+<div class ="btn-container">
+<button class ="start"> start </button>
+<button class ="stop"> stop </button>
+<button class = "reset" > reset </button>
+</div>
+</div>
+`;
